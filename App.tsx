561ec32f2ca61530bbe3f25e99108ffaa760bd4c
MacMac <r3ymarkolqph@gmail.com>
2024-08-20 13:07:27 +0800

    third commit

--- App.tsx
+++ App.tsx
@@ -1,53 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
-import CaptureScreen from './screens/CaptureScreen';
-import DiseaseDetectionScreen from './screens/DiseaseDetectionScreen';
-import { Colors } from './constants/Colors';
-import { useColorScheme } from './hooks/useColorScheme';
-import { TabBarIcon } from './navigation/TabBarIcon';
-
-const Tab = createBottomTabNavigator();
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const colorScheme = useColorScheme();
-
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme].tint,
-          headerShown: false,
-        }}
-      >
-        <Tab.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-            ),
-          }} 
-        />
-        <Tab.Screen 
-          name="Capture" 
-          component={CaptureScreen} 
-          options={{
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'camera' : 'camera-outline'} color={color} />
-            ),
-          }} 
-        />
-        <Tab.Screen 
-          name="Detection" 
-          component={DiseaseDetectionScreen} 
-          options={{
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
-            ),
-          }} 
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Opne up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto"/>
+    </View>
   );
+    
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- components/ThemedText.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, TextProps } from 'react-native';
-import { useThemeColor } from '../hooks/useThemeColor';
-
-export function ThemedText(props: TextProps) {
-  const { style, ...otherProps } = props;
-  const color = useThemeColor({}, 'text');
-
-  return <Text style={[{ color }, style]} {...otherProps} />;
-}
